c7a9b763be0fa0a5a6e86d06cb560d960e89571d
headweek <abdullayev233@gmail.com>
2025-03-09 17:25:53 +0400

    Initial Commit

---
+++ toDo/toDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoApp.swift
+//  toDo
+//
+//  Created by Salman Abdullayev on 09.03.25.
+//
+
+import SwiftUI
+
+@main
+struct toDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
